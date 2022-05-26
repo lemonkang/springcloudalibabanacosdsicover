4767ad212981c40500d9e4e74a15ebc39e960fad
kanghao <1084920778@qq.com>
2022-05-26 21:43:22 +0800

    nacosconfig的使用

---
+++ nacosconfig/src/main/java/com/kh/nacosconfig/controller/ProviderController.java
@@ -0,0 +1,19 @@
+package com.kh.nacosconfig.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/provider")
+public class ProviderController {
+
+    @Value("${user.name}")
+    private String serverPort;
+    @GetMapping("/reduce")
+    public String reduce(@RequestParam("num") String num){
+        return serverPort+"接口商品减少"+num;
+    }
+}
